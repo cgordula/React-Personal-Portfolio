95a3690b4ba1e4bacc5273d75063fe1e60ba233d
RevAdsCarmina <carmina@revenueads.com>
2023-04-10 22:58:53 -0500

    complete Nav.jsx and styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BiBook } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import { MdOutlineMessage } from 'react-icons/md';
+import { useState } from 'react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState('#');
+  return (
+    <section>
+      <nav>
+        <a
+          href="#home"
+          onClick={() => setActiveNav('#home')}
+          className={activeNav === '#home' ? 'active' : ''}
+        >
+          <AiOutlineHome />
+        </a>
+        <a
+          href="#about"
+          onClick={() => setActiveNav('#about')}
+          className={activeNav === '#about' ? 'active' : ''}
+        >
+          <AiOutlineUser />
+        </a>
+        <a
+          href="#experience"
+          onClick={() => setActiveNav('#experience')}
+          className={activeNav === '#experience' ? 'active' : ''}
+        >
+          <BiBook />
+        </a>
+        <a
+          href="#services"
+          onClick={() => setActiveNav('#services')}
+          className={activeNav === '#services' ? 'active' : ''}
+        >
+          <RiServiceLine />
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setActiveNav('#contact')}
+          className={activeNav === '#contact' ? 'active' : ''}
+        >
+          <MdOutlineMessage />
+        </a>
+      </nav>
+    </section>
+  );
 };
 
 export default Nav;
